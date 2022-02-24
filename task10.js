5d7c1d95411ec359696511f52dfdfaaec9aeb295
shamdzh <shamdzh@shamdzhs-MacBook-Pro.local>
2022-02-24 18:32:40 +0300

    update

--- task10.js
+++ task10.js
@@ -5,14 +5,43 @@ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses! */
 
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 function createPhoneNumber(numbers) {
-  let number = '';
+  for (let i = 0; i < numbers.length; i++) {
+    if (Number.isInteger(numbers[i])) {
+      continue;
+    } else return;
+  }
 
   if (numbers.length < 10 || numbers.length > 10) {
     return;
+  } else {
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
   }
-
-  
 }
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+console.log(createPhoneNumber(number));
+
+
+// best solution 
+function createPhoneNumber(numbers){
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') ' 
+      + numbers.substring(3, 6) 
+      + '-' 
+      + numbers.substring(6);
+}
+
+// or
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
